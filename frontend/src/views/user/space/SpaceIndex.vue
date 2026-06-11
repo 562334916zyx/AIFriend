89c1f7eebdbb57af86f461eab65a7be4079cc679
yvxin zhang <292683754@qq.com>
2026-06-11 20:30:25 +0800

    实现路由

---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {useRoute} from "vue-router";
+
+const router = useRoute();
+</script>
+
+<template>
+个人空间: {{ router.params.user_id}}
+</template>
+
+<style scoped>
+
+</style>
